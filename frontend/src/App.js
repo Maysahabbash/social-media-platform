059b7563a27d6193dc22d548a210ede56f87c1c0
maysahabbash92 <maysahabbash92@gmail.com>
2025-05-01 00:33:27 +0300

    Setup React

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
+function App() {
+  return (
+    <ChakraProvider>
+       <Router>
+         <Routes>
+
+
+           </Routes>
+        </Router>
+     </ChakraProvider>
+      
+      
+   
+  );
+}
+
+export default App;
