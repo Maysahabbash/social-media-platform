3008e08aed07c508de6915f413e6065486859686
maysahabbash92 <maysahabbash92@gmail.com>
2025-05-01 23:34:48 +0300

    Add API Endpoint for userprofile

--- frontend/src/routes/user_profile.jsx
+++ frontend/src/routes/user_profile.jsx
@@ -1,39 +1,46 @@
 import { Flex, Text, VStack, Box, Heading, HStack, Image, Button} from "@chakra-ui/react"; //chakra-ui DocS
 import { useEffect, useState } from "react";
+import { get_user_profile_data } from "../api/endpoints";
 
 const UserProfile = () => {
-
-   const get_username_from_url = () => {
-      const url_split = window.location.pathname.split('/');
-      return url_split[url_split.length-1]
-
-   }
-     
-    const [username, setUsername] = useState(get_username_from_url())
-    useEffect ( () => {
-      setUsername(get_username_from_url())
-
-    
-
- }, [] ) 
-
+  const get_username_from_url = () => {
+    const url_split = window.location.pathname.split('/');
+    return url_split[url_split.length-1];
+  }
+  
+  const [username, setUsername] = useState(get_username_from_url());
+  
+  useEffect(() => {
+    setUsername(get_username_from_url());
+  }, []); // Consider adding location dependency if URL changes matter
 
   return (
-    <Flex w='100' justifyContent={'center'}>
-         <VStack w='75%'>
-           <Box w='100%' mt='40px'>
-              <UserDetails/>
-           </Box>
-         </VStack>
+    <Flex w='100%' justifyContent={'center'}>
+      <VStack w='75%'>
+        <Box w='100%' mt='40px'>
+          <UserDetails username={username}/>
+        </Box>
+      </VStack>
     </Flex>
-  )
+  );
 }
+const UserDetails = ({username}) => {
 
-const UserDetails = () => {
+  useEffect(() =>{
+    const fetchData = async () => {
+      const data = await get_user_profile_data(username); // Fixed variable name
+      console.log(data);
+    
+    }
+    fetchData()    
+
+  }, [])
+
+
   return(
     
      <VStack  w='100%' alignItems={'start'} gap={'35px'}>
-      <Heading>@Maysahabbash</Heading>
+      <Heading>@{username}</Heading>
       <HStack gap='20px'> 
         <Box boxSize='150px' border={'1px solid'}borderRadius={'full'} borderColor={'gray.700'} bg='white' overflow={'hidden'}>
           <Image src="http://127.0.0.1:8000/api/media/profile_image/32ed91aa83d19e1251ea833a7f736d1b.jpg"  boxSize={"100%"} objectFit={"cover"}/>
