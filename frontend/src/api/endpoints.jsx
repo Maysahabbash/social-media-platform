f55767bdaa3437ac15c81d9e10c7bdda77e7c3ca
maysahabbash92 <maysahabbash92@gmail.com>
2025-05-02 21:04:34 +0300

    Add login endpoint

--- frontend/src/api/endpoints.jsx
+++ frontend/src/api/endpoints.jsx
@@ -10,8 +10,53 @@ const api = axios.create({
 
 })
 
+api.interceptors.response.use(
+   (response) => response,
+
+   async error => {
+    const original_request = error.config
+
+    if(error.response?.status === 401 && !original_request._retry){
+        original_request._retry = true;
+
+        try{
+            await refresh_token();
+            return api(original_request);
+
+        } catch (refreshError){
+            window.location.herf = '/login'
+            return Promise.reject(refreshError)
+        }
+        
+
+
+    }
+    return Promise.reject(refreshError)
+
+
+
+   }
+   
+   
+)
 export const get_user_profile_data = async (username) => {
     const response = await api.get(`/user_data/${username}/`);
     return response.data
 
+}
+
+export const refresh_token = async () => {
+    const response = await api.post('/token/refresh/');
+    return response.data
+
+
+    
+}
+
+export const login = async (username, password) => {
+    const response = await api.post('/token/',{username, password});
+    return response.data
+
+
+    
 }
